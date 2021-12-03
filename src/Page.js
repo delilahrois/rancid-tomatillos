5e851806ea7f6996398a3d2901150117d24a7d0f
Michele Comfort <michelecomfort@gmail.com>
2021-12-02 22:18:48 -0700

    Begin formatting page component

--- src/Page.js
+++ src/Page.js
@@ -4,13 +4,18 @@ import './Page.css'
 const Page = ({movie, returnToMain}) => {
   // console.log(movie)
   return (
-    <div className='movie-page'>
-      <button onClick={() =>returnToMain()}>Return to All Movies!</button>
-      <img src={movie.backdrop_path} />
-      <h2>{movie.title}</h2>
-      <h3>Audience Rating: {Math.round(movie.average_rating)}</h3>
-      <p>Movie Plot: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-    </div>
+    <section className='movie-page'>
+      <img className='page-img'src={movie.backdrop_path} />
+      <article className='side-info'>
+        <button onClick={() =>returnToMain()}>Return to All Movies!</button>
+        <h2>{movie.title}</h2>
+        <h3>Audience Rating: {Math.round(movie.average_rating)}</h3>
+        <p>Movie Plot: Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever since
+        the 1500s, when an unknown printer took a galley of type and scrambled it
+        to make a type specimen book.</p>
+      </article>
+    </section>
   )
 }
 

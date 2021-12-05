4720512071ac43eff36209c3b23b931ae66a810c
Delilah Necrason <delilahnecrason@gmail.com>
2021-12-05 13:56:27 -0500

    Tinker with css styling

--- src/Page.js
+++ src/Page.js
@@ -7,13 +7,13 @@ const Page = ({ movie, returnToMain }) => {
     <section className='movie-page'>
       <img className='page-img'src={movie.backdrop_path} />
       <article className='side-info'>
-        <button onClick={() =>returnToMain()}>Return to All Movies!</button>
-        <h2>{movie.title}</h2>
-        <h3>Audience Rating: {Math.round(movie.average_rating)}</h3>
-        <p>Movie Plot: Lorem Ipsum is simply dummy text of the printing and typesetting
+        <h2 className="movie-title">{movie.title}</h2>
+        <h3 className="movie-rating">Audience Rating: {Math.round(movie.average_rating)}</h3>
+        <p className="movie-plot">Movie Plot: Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever since
         the 1500s, when an unknown printer took a galley of type and scrambled it
         to make a type specimen book.</p>
+        <button onClick={() =>returnToMain()}>Return to All Movies!</button>
       </article>
     </section>
   )

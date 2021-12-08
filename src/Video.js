326d45b209deb99308da6259131bdd7c06070f05
Michele Comfort <michelecomfort@gmail.com>
2021-12-08 10:56:44 -0700

    Add conditional rendering based on video src player to render proper video format

--- src/Video.js
+++ src/Video.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Video = (trailer) => {
-
-
 console.log(trailer.trailer.site)
+let videoURL;
+{trailer.trailer.site === 'YouTube' ? videoURL = (`https://www.youtube.com/embed/${trailer.trailer.key}`) : videoURL = (`https://player.vimeo.com/video/${trailer.trailer.key}`)}
   return (
     <div className="video-responsive">
      <iframe
        width="400"
        height="240"
-       src={`https://www.youtube.com/embed/${trailer.trailer.key}`}
+       src={videoURL}
        frameBorder="0"
        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
        allowFullScreen

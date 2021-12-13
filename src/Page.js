274ac2c1d6ff11a21df00dda6b62b15bd9f5c11f
Delilah Necrason <delilahnecrason@gmail.com>
2021-12-13 15:39:14 -0500

    Move return button on Page

--- src/Page.js
+++ src/Page.js
@@ -10,11 +10,13 @@ const Page = ({ movie, overview, trailer }) => {
     <section className='movie-page'>
       <img className='page-img'src={movie.backdrop_path} alt={movie.title}/>
       <article className='side-info'>
-        <h2 className="movie-title">{movie.title}</h2>
+        <div className="title-container">
+          <h2 className="movie-title">{movie.title}</h2>
+          <Link to="/" style={{textDecoration: 'none'}} onClick={() => {this.props.refresh()}}><button className='return-button'>Return to All Movies!</button></Link>
+        </div>
         <h3 className="movie-rating">Audience Rating: {Math.round(movie.average_rating)}</h3>
         <Video trailer={trailer}/>
         <p className="movie-plot">{overview}</p>
-        <Link to="/" style={{textDecoration: 'none'}} onClick={() => {this.props.refresh()}}><button className='return-button'>Return to All Movies!</button></Link>
       </article>
     </section>
   )

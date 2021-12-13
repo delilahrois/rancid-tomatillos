15207f61c120a253eb8fa8356f3ee84758764919
Michele Comfort <michelecomfort@gmail.com>
2021-12-13 16:31:33 -0700

    Render inputs only on main page

--- src/Components/App.js
+++ src/Components/App.js
@@ -49,7 +49,7 @@ class App extends Component {
     }
   }
 
-  refreshState = () => {
+  refreshState =  () => {
     this.setState(
       {
         movies: [],
@@ -68,13 +68,13 @@ class App extends Component {
       <main>
         <header>
           <NavLink to={`/`} style={{textDecoration: 'none'}} onClick={() => {this.refreshState()}}>
-            <h1 className="header">Rancid T🍅matillos</h1>
+            <h1 className="header">Rancid Tomatillos</h1>
           </NavLink>
           <p className='error-message'>{this.state.error}</p>
-          <Input setSingleMovie={this.setSingleMovie} setFilteredMovies={this.setFilteredMovies} movies={this.state.movies} refresh={this.refreshState}/>
+
         </header>
         <Routes>
-          <Route path="/" element={<Library movies={this.state.movies} displayMovie={this.displayMovie} foundMovies={this.state.foundMovies}/>}/>
+          <Route path="/" element={<><Input setSingleMovie={this.setSingleMovie} setFilteredMovies={this.setFilteredMovies} movies={this.state.movies} refresh={this.refreshState}/><Library movies={this.state.movies} displayMovie={this.displayMovie} foundMovies={this.state.foundMovies}/></>}/>
           <Route path="/:movieId" element={<Page movie={this.state.singleMovie}
             trailer={this.state.trailer} overview={this.state.movieOverview} key={this.state.singleMovie.id} />}/>
         </Routes>

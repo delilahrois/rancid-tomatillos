5f67e554a36d3d5b66328f430c3fe016985b9169
Michele Comfort <michelecomfort@gmail.com>
2021-12-13 16:46:43 -0700

    Add space to bottom of grid

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Video from './Video'
 import '../CSS/Page.css';
 
-const Page = ({ movie, overview, trailer }) => {  
+const Page = ({ movie, overview, trailer }) => {
   return (
     <section className='movie-page'>
       <img className='page-img'src={movie.backdrop_path} alt={movie.title}/>
@@ -12,7 +12,7 @@ const Page = ({ movie, overview, trailer }) => {
           <h2 className="movie-title">{movie.title}</h2>
           <Link to="/" style={{textDecoration: 'none'}} onClick={() => {this.props.refresh()}}><button className='return-button'>Return to All Movies!</button></Link>
         </div>
-        <h3 className="movie-rating">Audience Rating: {Math.round(movie.average_rating)}</h3>
+        <h3 className="movie-rating">Audience Rating: {Math.round(movie.average_rating)}/10</h3>
         <Video trailer={trailer}/>
         <p className="movie-plot">{overview}</p>
       </article>
